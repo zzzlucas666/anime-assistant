bddd9269cfb7c8228096a8a1dfa9a216f075c70f
zzzlucas666 <3532797043@qq.com>
2026-06-18 10:50:50 +0800

    Create application entry point

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+def main():
+    print("Anime Assisitant starting...")
 
 
 def print_hi(name):
@@ -11,6 +13,5 @@ def print_hi(name):
 
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    main()
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
